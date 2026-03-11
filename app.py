e294e97bb21541b9b7eb925de5e7f87eaf2e5c8f
Alceu Pereira <alceupereira3105@gmail.com>
2026-03-11 00:36:46 -0300

    Servindo o API no HTML

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from database import conn, cursor
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/app")
+def frontend():
+    return FileResponse("frontend/index.html")
 
 @app.get("/")
 def home():
---
+++ static/script.js
@@ -0,0 +1,40 @@
+const API = "http://127.0.0.1:8000"
+
+async function carregarTarefas() {
+    
+    let resposta = await fetch(API + "/tarefas")
+    let tarefas = await resposta.json()
+
+    let lista = document.getElementById("lista")
+    lista.innerHTML = ""
+
+    tarefas.array.forEach(t => {
+        
+        let item = document.createElement("li")
+        item.innerHTML = t[1] + " "
+
+        let btn = document.createElement("button")
+        btn.innerText = "Excluir"
+
+        btn.onclick = () => deletarTarefa(t[0])
+
+        item.appendChild(btn)
+
+        lista.appendChild(item)
+    });
+}
+
+async function criarTarefas() {
+    let titulo = document.getElementById("titulo").value
+    await fetch(API + "/tarefas?titulo=" + titulo, {method: "POST"})
+
+    carregarTarefas()
+}
+
+async function deletarTarefa(id) {
+    await fetch(API + "/tarefas/" + id, {method: "DELETE"})
+
+    carregarTarefas()
+}
+
+carregarTarefas()
